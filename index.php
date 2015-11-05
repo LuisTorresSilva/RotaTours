b1500e200be02c6dd9a5b44c1ba45e5fbeccac1c
LuisTorresSilva <myvirus@gmail.com>
2015-11-05 22:25:05 +0000

    RotaTours Landing Page

---
+++ index.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        
+        <title>Rotatours</title>
+        
+        <meta charset="utf-8">
+        <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <meta name="description" content="">
+        
+        <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimun-scale=1.0, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+        <link rel="stylesheet" href="css/reset.css">
+        <!--<link rel="stylesheet" href="css/normalize.min.css">-->
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        
+        <!-- Medium and Small -->
+		<link href="css/main_medium.css" rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width:1000px)">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        
+        
+        
+    </head>
+
+    <body>
+        
+        
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+
+        <div class="main-container">
+            
+            <div class="main wrapper clearfix">
+                
+                <div class="header-container">
+                    
+                    <div class="logoSmallHeader">
+                                
+                    </div> <!--#logoSmallHeader-->
+                                       
+                    <header class="header-Menu">
+                        <nav>
+                            <ul>
+                                <li><a href="#">HOME</a></li>
+                                <li><a href="#">TOURS</a></li>
+                                <li><a href="#">SERVICES</a></li>
+                                <li><a href="#">ABOUT US</a></li>
+                                <li><a href="#">CONTACT US</a></li>
+                            </ul>
+                        </nav>
+                        
+                        <div class="searchWrapper">
+                        
+                            <form class="searchForm" action="#">
+
+                                <input type="text" name="search"><br>
+                                <!--<input type="submit" value="Submit">-->
+                                <!--<button type="submit"><img src="../img/magnifyingglass.png" border="0" /></button>-->
+                                
+                                <div class="lupa">
+                                    
+                                    <a href="#"><img src="img/magnifyingglass.png" alt="shearchIcon" /></a>
+                                    
+                                </div> <!--#lupa-->
+
+                            </form>
+                            
+                            <div class="flags">
+
+                                <div class="flagPTWrapper">
+
+                                    <a class="flagPT" href="#"><img src="img/flagPT.png" alt="flagPT" /></a> 
+
+                                </div>
+
+                                <div class="flagUKWrapper">
+
+                                    <a class="flagUK" href="#"><img src="img/flagUK.png" alt="flagUK" /></a>
+
+                                </div>
+
+                            </div> <!--#flags-->
+                            
+                        </div> <!--#searchWrapper-->
+                        
+                    </header>
+                    
+                <div class="clear"></div>
+                    
+                </div> <!--#header-container-->
+                
+                <div class="slideshowImageWrapper">
+                    
+                    <div class="captionWrapper">
+                        <div class="caption">
+                            <h2>OLD LISBON VIEW</h2>
+                            <p class="parag">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin, nibh quis suscipit euismod, dui justo laoreet magna, at accumsan tortor mi et neque.</p> <!--#parag-->
+                        </div> <!--#caption-->
+                    </div> <!--#captionWrapper-->
+                                
+                </div> <!--#slideshowImageWrapper-->
+                
+                <div class="clear"></div>
+                                           
+                <article>
+                    
+                    <div class="h1HeaderWrapper">
+                        <h1 class="h1Header">Welcome to RotaTours Portugal</h1>
+                    </div> <!--#h1HeaderWrapper-->
+                    
+                    <section>
+
+                    </section>
+                 
+                </article>
+
+            </div> <!-- #main -->
+                 
+        </div> <!-- #main-container -->
+        
+        
+
+        <!--<div class="footer-container">
+            <footer class="wrapper">
+                <h3>footer</h3>
+            </footer>
+        </div>-->       
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+        
+    </body>
+
+</html>
